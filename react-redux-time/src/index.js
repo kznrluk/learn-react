cefe8f5d66bfed487691d58b3eb640ff9f783677
kznrluk <kznr.luk@gmail.com>
2018-02-07 18:23:31 +0900

    とりあえず動作

--- react-redux-time/src/index.js
+++ react-redux-time/src/index.js
@@ -4,19 +4,28 @@ import { createStore } from 'redux'
 import { Provider, connect } from "react-redux";
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import unixtotime from './time.js'
 
+const initTime = new Date().getTime()
 const init = {
     text: "HELLO",
-    count: 2
+    count: 2,
+    time: initTime
 }
 
 // REDUSER
+const ADD_STAR = "ADD_STAR";
+const RELOAD_TIME = "RELOAD_TIME";
 const STARS = (state = init, action) => {
     switch(action.type){
-        case 'ADD_STAR':
+        case ADD_STAR:
             console.log(state.count)
             return Object.assign({}, state, {
-               count: state.count * state.count
+               count: state.count * 2
+            });
+        case RELOAD_TIME:
+            return Object.assign({}, state, {
+                time: action.value
             });
         default:
             return Object.assign({}, state, {
@@ -26,7 +35,6 @@ const STARS = (state = init, action) => {
 
 let store = createStore(STARS);
 
-const ADD_STAR = "ADD_STAR"
 
 // ACTION
 const addStar = () => {
@@ -34,8 +42,36 @@ const addStar = () => {
         type: ADD_STAR
     }
 }
+const CHANGE_TIME = () => {
+    const nowTime = new Date().getTime()
+    return {
+        type: RELOAD_TIME,
+        value: nowTime
+    }
+}
+
+setInterval(()=>{
+    store.dispatch(CHANGE_TIME());
+    console.log('changed')
+},1000)
 
 console.log(store.getState().count)
+
+class Clock extends React.Component {
+    constructor(props){
+        super(props)
+        console.log(this.props.time)
+    }
+    render (){
+        const time = unixtotime(this.props.time / 1000)
+        return(
+            <div>
+                <p>NOW: {time}</p>
+                <p>UNIX: {this.props.time}</p>
+            </div>
+        )
+    }
+}
 
 
 class View extends React.Component {
@@ -56,6 +92,7 @@ class View extends React.Component {
         }
         return (
             <div>
+                <Clock time={this.props.time}/>
                 <p>HELLO{stars}</p>
                 <button onClick={this.onClick}>☆</button>
             </div>
